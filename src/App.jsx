78b5f82dc5dc8f3b61d5ca82abfb96fcf030adae
Maria <wolfmari57@gmail.com>
2023-08-13 23:58:54 -0600

    se agrego 2 hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,11 @@ import './App.css';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 
-const tasks = [
-  { name: 'Lavar los trastes', completed: true },
-  { name: 'Ordenar mi cuarto', completed: false },
-  { name: 'Hacer ejercicio', completed: true },
-  // ...add more tasks here
-];
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <TaskList tasks={tasks} />
+      <TaskList />
     </div>
   );
 }
